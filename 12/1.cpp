228e8a417fbae0b98ed65a4bd4a7ed676fd56794
24siefil <24siefil@gmail.com>
2022-09-30 21:32:01 +0900

    add: week1

---
+++ 12/1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string str;
+int summary = 0;
+
+int main(void) {
+    cin >> str;
+
+    for (int i = 0; i < str.size() / 2; i++) {
+        summary += str[i] - '0';
+    }
+    for (int i = str.size() / 2; i < str.size(); i++) {
+        summary -= str[i] - '0';
+    }
+
+    if (summary == 0) {
+      cout << "LUCKY" << '\n'
+    } else {
+      cout << "READY" << '\n';
+    }
+}
